bb064b4f6d37db90838931889f90cc41dafa8d7c
Mary <marybauman503@gmail.com>
2024-06-29 14:30:36 -0400

    find

--- py/compProgramming.py
+++ py/compProgramming.py
@@ -7,9 +7,20 @@ html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 
 sectionHeads = soup.find_all('span', attrs={'class': 'mw-headline'})
-print("\nSection Heads")
-for sectionHead in sectionHeads: print(sectionHead.text)
+# print("\nSection Heads")
+# for sectionHead in sectionHeads: print(sectionHead.text)
 
-titles = soup.find_all(['title', 'h1', 'h2'])
-print("\nTitles")
-for t in titles: print(t.text)
+#mw-default-size
+mw = soup.find(class_="mw-default-size")
+# print(mw)
+# print(mw.text)
+# print(mw.contents)
+# print(mw.attrs)
+for child in mw.children:
+    print("\nchild")
+    print(child)
+
+
+# titles = soup.find_all(['title', 'h1', 'h2'])
+# print("\nTitles")
+# for t in titles: print(t.text)
