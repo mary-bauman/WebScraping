d0e65298fe5a10bf7b9fb66e04368b0ab10af295
Mary <marybauman503@gmail.com>
2024-07-01 20:12:07 -0400

    10

--- py/2048-wiki.py
+++ py/2048-wiki.py
@@ -3,3 +3,11 @@ import requests
 
 response = requests.get('https://en.wikipedia.org/wiki/2048_(video_game)')
 soup = BeautifulSoup(response.text, 'html.parser')
+
+#text of top level heading (h1)
+
+
+#how many second level headings (h2)
+
+
+#extract href of first link on page
--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
 zenva course https://academy.zenva.com/course/intermediate-web-scraping-and-data-cleaning-with-python/
-status: finished lesson 9/13
+status: finished lesson 10/13
