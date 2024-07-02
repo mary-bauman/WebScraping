8c5c2ce12f2f89a23a33a4291f9033c7172f4c41
Mary <marybauman503@gmail.com>
2024-07-01 20:21:53 -0400

    10 challenges done

--- py/2048-wiki.py
+++ py/2048-wiki.py
@@ -5,9 +5,14 @@ response = requests.get('https://en.wikipedia.org/wiki/2048_(video_game)')
 soup = BeautifulSoup(response.text, 'html.parser')
 
 #text of top level heading (h1)
-
+h1 = soup.find('h1')
+print(h1.text)
 
 #how many second level headings (h2)
+h2s = soup.find_all('h2')
+print(len(h2s))
 
 
 #extract href of first link on page
+firstLink = soup.find('a')['href']
+print(firstLink)
